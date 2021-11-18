e3f16c86f6c6c12aef475e89ffd7366bc5e12043
Gidon Tobin <62831715+gidontobin@users.noreply.github.com>
2021-11-18 15:01:42 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/AP.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "AP.exe"
+  "AP.exe.manifest"
+  "AP.lib"
+  "AP.pdb"
+  "CMakeFiles/AP.dir/SimpleAnomalyDetector.cpp.obj"
+  "CMakeFiles/AP.dir/anomaly_detection_util.cpp.obj"
+  "CMakeFiles/AP.dir/main.cpp.obj"
+  "CMakeFiles/AP.dir/timeseries.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
